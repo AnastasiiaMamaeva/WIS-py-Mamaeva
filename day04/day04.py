d9d9f23fba77e9da0ac4a6ece26a907f04670765
AnastasiiaMamaeva <mamaeva.anastas@gmail.com>
2024-11-26 23:23:59 +0200

    commit

--- day04/day04.py
+++ day04/day04.py
@@ -4,39 +4,46 @@ import random
 
     
 
-correct = False
-again = True
-while again == True: 
+def check_input(user_input, random_number, attempts):
+    if random_number < int(user_input):
+        print("Oh, your number is too big, try again!")
+    elif random_number > int(user_input):
+        print("Oh, your number is too small, try again!")
+    elif random_number == int(user_input):
+        print("Good job! You guessed right! You did", attempts, "tries!", sep=" ")
+        return True
+    return False
+
+
+def main():
+    correct = False
     random_number = random.randint(1, 20)
-    i = 1
-    while correct == False:
-      user_num = input("try to guess a number between 1 and 20")
-      if user_num == "x":
-        again = False
-        break
-      elif user_num == "n":
-        if input("Do you want to play a new gaim?") == "Yes":
-          break
-      elif user_num == "s":
-          print("The numbr is", random_number, sep = " ")
-      elif random_number < int(user_num):
-          print("Oh, your number is too big, try again!")
-      elif random_number > int(user_num):
-          print("Oh, your number is too small, try again!")
-      elif random_number == int(user_num):
-          print("Good job! You guessed right! You did", i, "tries!", sep = " ")
-          correct = True
-          ask = input("Do you want to play again? (type 'Yes' or 'No')")
-          if ask == "Yes":
-            break
-          elif ask == "x":
-              again = False
-              break
-          elif ask == "n":
-            if input("Do you want to play a new gaim?") == "Yes":
-              break
-          elif ask == "s":
-            print("The numbr is", random_number, sep = " ")
-          else:
-            again = False
-      i +=1
+    attempts = 1
+
+    while not correct:
+        user_input = input("Try to guess a number between 1 and 20: ")
+        if user_input == "x":
+            return False
+        elif user_input == "s":
+            print("The number is", random_number, sep=" ")
+        elif user_input == "n":
+            if input("Do you want to play a new game? (Yes/No): ").lower() == "yes":
+                return True
+        elif user_input.isdigit():
+            correct = check_input(user_input, random_number, attempts)
+            if correct:
+                if input("Do you want to play again? (type 'Yes' or 'No'): ").lower() != "yes":
+                    return False
+        else:
+            print("Invalid input. Please enter a number, 's', 'n', or 'x'.")
+        attempts += 1
+
+
+def play_game():
+    play_again = True
+    while play_again:
+        play_again = main()
+
+
+if __name__ == "__main__":
+    play_game()
